a26894aed518645346f159898fbfb3dd8977d41d
MeltoE <36153733+MeltoE@users.noreply.github.com>
2019-05-05 22:43:54 +0300

    javascript toimii, lispäivitetty scripti etusivulle

--- frontend/js/displayproducts.js
+++ frontend/js/displayproducts.js
@@ -5,29 +5,63 @@ window.onload = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             var notparsed = xmlhttp.responseText;
             var parsed = JSON.parse(notparsed);
-            var kuvat = [];
+            var tuotteet = [];
             var string = "Kuvat: ";
             parsed.forEach(obj => {
                 //string = string + ' ' + obj.kuva;
                 var parse = JSON.parse(obj);
                 string = string + ' ' + parse.kuva;
-                kuvat.push(parse.kuva);
+                tuotteet.push(parse);
             });
             document.getElementById("images").innerHTML = string;
-            createProduct(kuvat);
+            createProduct(tuotteet);
         }
     };
-    xmlhttp.open("GET", "../../php/getImage.php", true);
+    xmlhttp.open("GET", "../../php/getAlltuoteinfo.php", true);
     xmlhttp.send();
 };
 //funktio joka luo kuvat sivulle ja tarkoitus laajentaa että luo vasemmalta oikealle kaikista tuotteista kuvat
-function createProduct(kuvat) {
+function createProduct(tuotteet) {
 
-    kuvat.forEach(kuva => {
-        var divelement = document.createElement("div");
-        var imgelement = document.createElement("img");
-        imgelement.setAttribute("src", kuva);
-        divelement.appendChild(imgelement);
-        document.getElementById("myForm").appendChild(divelement);
+    var olproducts = document.createElement("ol");
+
+    tuotteet.forEach(tuote => {
+        var div = document.createElement("div");
+        var img = document.createElement("img");
+        var h1 = document.createElement("h1");
+        var price = document.createElement("price");
+        var details = document.createElement("p");
+        var pforb = document.createElement("p");
+        var button = document.createElement("button");
+        pforb.appendChild(button);
+
+        var a = [img,h1,price,details,pforb];
+
+        for (let i = 0; i < a.length; i++) {
+            div.appendChild(a[i]);
+        }
+
+        div.setAttribute("class","card");
+
+        img.setAttribute("src",tuote.Kuva);
+        img.setAttribute("width",200);
+        img.setAttribute("height",200);
+
+        h1.innerText = tuote.Nimi;
+
+        price.setAttribute("class","price");
+        price.innerText = tuote.Hinta;
+
+        details.innerText = "Tuotekuvaus";
+
+        button.innerText = "Add to Cart";
+
+        var list = document.createElement("li");
+
+        list.appendChild(div);
+        olproducts.appendChild(list);
+
+
     });
+    document.getElementById("tuotteet").appendChild(olproducts);
 };
