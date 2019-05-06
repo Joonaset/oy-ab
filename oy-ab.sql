533839b5dfd5cd07d23fab255f868a530774aef1
MeltoE <36153733+MeltoE@users.noreply.github.com>
2019-05-06 15:57:07 +0300

    ääkköset muutettu

--- oy-ab.sql
+++ oy-ab.sql
@@ -11,35 +11,35 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Dumping structure for taulu oy-ab.käyttäjä
-CREATE TABLE IF NOT EXISTS `käyttäjä` (
-  `Sähköposti` varchar(50) NOT NULL,
+-- Dumping structure for taulu oy-ab.kayttaja
+CREATE TABLE IF NOT EXISTS `kayttaja` (
+  `Sahkoposti` varchar(50) NOT NULL,
   `Salasana` varchar(50) NOT NULL,
   `Osoite` varchar(50) NOT NULL,
   `Oikeudet` int(11) NOT NULL,
   `Tilausnumero` int(11) NOT NULL,
-  PRIMARY KEY (`Sähköposti`),
+  PRIMARY KEY (`Sahkoposti`),
   KEY `Tilausnumero` (`Tilausnumero`),
-  CONSTRAINT `käyttäjä_ibfk_1` FOREIGN KEY (`Tilausnumero`) REFERENCES `tilaus` (`Tilausnumero`)
+  CONSTRAINT `kayttaja_ibfk_1` FOREIGN KEY (`Tilausnumero`) REFERENCES `tilaus` (`Tilausnumero`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table oy-ab.käyttäjä: ~0 rows (suunnilleen)
-/*!40000 ALTER TABLE `käyttäjä` DISABLE KEYS */;
-/*!40000 ALTER TABLE `käyttäjä` ENABLE KEYS */;
+-- Dumping data for table oy-ab.kayttaja: ~0 rows (suunnilleen)
+/*!40000 ALTER TABLE `kayttaja` DISABLE KEYS */;
+/*!40000 ALTER TABLE `kayttaja` ENABLE KEYS */;
 
--- Dumping structure for taulu oy-ab.sisältää
-CREATE TABLE IF NOT EXISTS `sisältää` (
+-- Dumping structure for taulu oy-ab.sisaltaa
+CREATE TABLE IF NOT EXISTS `sisaltaa` (
   `Tilausnumero` int(11) NOT NULL,
   `Tuotetunnus` int(11) NOT NULL,
   PRIMARY KEY (`Tilausnumero`,`Tuotetunnus`),
   KEY `Tuotetunnus` (`Tuotetunnus`),
-  CONSTRAINT `sisältää_ibfk_1` FOREIGN KEY (`Tilausnumero`) REFERENCES `tilaus` (`Tilausnumero`),
-  CONSTRAINT `sisältää_ibfk_2` FOREIGN KEY (`Tuotetunnus`) REFERENCES `tuote` (`Tuotetunnus`)
+  CONSTRAINT `sisaltaa_ibfk_1` FOREIGN KEY (`Tilausnumero`) REFERENCES `tilaus` (`Tilausnumero`),
+  CONSTRAINT `sisaltaa_ibfk_2` FOREIGN KEY (`Tuotetunnus`) REFERENCES `tuote` (`Tuotetunnus`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Dumping data for table oy-ab.sisältää: ~0 rows (suunnilleen)
-/*!40000 ALTER TABLE `sisältää` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sisältää` ENABLE KEYS */;
+-- Dumping data for table oy-ab.sisaltaa: ~0 rows (suunnilleen)
+/*!40000 ALTER TABLE `sisaltaa` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sisaltaa` ENABLE KEYS */;
 
 -- Dumping structure for taulu oy-ab.tilaus
 CREATE TABLE IF NOT EXISTS `tilaus` (
@@ -81,8 +81,8 @@ CREATE TABLE IF NOT EXISTS `tuotetyyppi` (
 -- Dumping data for table oy-ab.tuotetyyppi: ~2 rows (suunnilleen)
 /*!40000 ALTER TABLE `tuotetyyppi` DISABLE KEYS */;
 INSERT INTO `tuotetyyppi` (`Nimi`, `Kuvaus`, `Hinta`, `Tuotetunnus`, `Kuva`) VALUES
-	('Meisseli', 'Työkalu', 10, 1, '../kuvat/meisseli.jpg'),
-	('Vasara', 'Työkalu', 5, 2, '../kuvat/vasara.jpg');
+	('Meisseli', 'Tyokalu', 10, 1, '../kuvat/meisseli.jpg'),
+	('Vasara', 'Tyokalu', 5, 2, '../kuvat/vasara.jpg');
 /*!40000 ALTER TABLE `tuotetyyppi` ENABLE KEYS */;
 
 -- Dumping structure for taulu oy-ab.varasto
