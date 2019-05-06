ea8fbd9deff5887a7b0b08c2a877ac8a8cdbb4b4
MeltoE <36153733+MeltoE@users.noreply.github.com>
2019-05-06 21:58:01 +0300

    php tuote haku kuvauksella

---
+++ php/getTuotebykuvaus.php
@@ -0,0 +1,22 @@
+<?php
+include "DBController.php";
+$db_handle = new DBController();
+
+$sql = "SELECT * FROM tuotetyyppi WHERE kuvaus = ?";
+
+//prepared statements
+$prepare = $db_handle->db()->prepare($sql);
+
+//$_POST saa parametrin formista ja on näkymätön muille
+$prepare->bind_param( $_POST['kuvaus']);
+
+$prepare->execute();
+$result = $prepare->get_result();
+
+while ($row = mysqli_fetch_assoc($result)) {
+    $productinfo[] = json_encode($row);
+}
+
+$encoded = json_encode($productinfo);
+echo $encoded;
+?>
