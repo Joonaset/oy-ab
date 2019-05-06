38a71c38d6f72fc48a3a7c58482c3d0f1f295a01
MeltoE <36153733+MeltoE@users.noreply.github.com>
2019-05-06 22:32:25 +0300

    ätuotteidenlajittelu päivitetty toimivaksi

--- frontend/js/tuotteidenlajittelu.js
+++ frontend/js/tuotteidenlajittelu.js
@@ -6,7 +6,7 @@ function Tuotelajittelija(tuotteet) {
 
     this.lajitteleVaraosat = function () {
         this.tuotteet.forEach(obj =>{
-            if (obj.Kuvaus == "Varaosa") {
+            if (obj.Kuvaus === "Varaosa") {
                 this.varaosat.push(obj);
             }
             else {
@@ -16,7 +16,7 @@ function Tuotelajittelija(tuotteet) {
     };
     this.lajitteleTarvikkeet = function () {
         this.tuotteet.forEach(obj =>{
-            if (obj.Kuvaus == "Tyokalu" || obj.kuvaus == "Iso kone") {
+            if (obj.Kuvaus === "Tyokalu" || obj.Kuvaus === "Iso kone") {
                 this.tarvikkeet.push(obj);
             }
             else{
@@ -26,7 +26,7 @@ function Tuotelajittelija(tuotteet) {
     };
     this.lajitteleMuut = function () {
         this.tuotteet.forEach(obj =>{
-            if (obj.Kuvaus == "Muut tuotteet") {
+            if (obj.Kuvaus === "Muut tuotteet") {
                 this.muut.push(obj);
             }
             else {
@@ -43,4 +43,53 @@ function Tuotelajittelija(tuotteet) {
     this.getMuut = function () {
         return this.muut;
     };
+
+    this.makeProduct = function (tuotteet) {
+        var olproducts = document.createElement("ol");
+        tuotteet.forEach(tuote => {
+            var listproducts = document.createElement("li");
+            var divelistproducts = document.createElement("div");
+            var spane = document.createElement("span");
+
+            var divlistproductinfo = document.createElement("div");
+
+            //divlistproductinfo <-- a <-- div <-- div <-- div <-- img
+
+            var athumbnaillink = document.createElement("a");
+
+            var divthumbnail = document.createElement("div");
+            var divthumbnailcontainer = document.createElement("div");
+            var img = document.createElement("img");
+            img.setAttribute("src", tuote.Kuva);
+            img.setAttribute("width", 198);
+            img.setAttribute("height", 202);
+
+            divthumbnailcontainer.appendChild(img);
+            divthumbnail.appendChild(divthumbnailcontainer);
+            athumbnaillink.appendChild(divthumbnail);
+
+            //divlistproductinfo <-- div <-- a div <-- div <-- span
+            var divproductinfo = document.createElement("div");
+            var aproductinfolink = document.createElement("a");
+            aproductinfolink.innerText = tuote.Nimi;
+            var divproductprice = document.createElement("div");
+            var divprice = document.createElement("div");
+            var spanprice = document.createElement("span");
+            spanprice.innerText = tuote.Hinta;
+
+            divprice.appendChild(spanprice);
+            divproductprice.appendChild(divprice);
+            divproductinfo.appendChild(aproductinfolink);
+            divproductinfo.appendChild(divproductprice);
+
+            //next
+            divlistproductinfo.appendChild(athumbnaillink);
+            divlistproductinfo.appendChild(divproductinfo);
+
+            divelistproducts.appendChild(divlistproductinfo);
+            listproducts.appendChild(divelistproducts);
+            olproducts.appendChild(listproducts);
+        });
+        document.getElementById("varaosat-lista").appendChild(olproducts);
+    }
 }
