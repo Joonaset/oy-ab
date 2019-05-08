09fc03c483fd357864d6a3f2a28bf672b4681bde
Joonas Taivalmaa <Joonas.Taivalmaa@metropolia.fi>
2019-05-08 14:18:41 +0300

    login tallentaa userin

--- php/login.php
+++ php/login.php
@@ -1,32 +1,29 @@
 <?php
+// login.php returns json format of
 include 'DBController.php';
 $db_handle = new DBController();
 
-$ID = "testaaja@testi.fi";//$_POST['email'];
-$PASS = "sali";//$_POST['pass'];
+$ID = $_POST['email'];
+$PASS = $_POST['pass'];
 $AUTH = base64_encode($ID.$PASS);
 
 $sql = "SELECT * FROM kayttaja WHERE Salasana = ?";
 if (!($prepare = $db_handle->db()->prepare($sql))) { //Returns true if preparation does not work
-    echo "Preparation failed";
 }
 $prepare->bind_param("s", $AUTH);
 if (!($prepare->execute())) {
-    echo "MySQLi execution failed";
 }
 $result = $prepare->get_result();
 if ($result->num_rows === 0) {
-    echo "Väärä salasana";
+    echo "Väärä käyttäjätunnus tai salasana";
     return false;
 }
 while ($row = mysqli_fetch_assoc($result)) {
     $user[] = json_encode($row);
 }
-
 $encoded = json_encode($user);
-print $encoded;
-echo "Olet kirjautunut sisään";
+echo "<script>localStorage.setItem('user', $ID)</script>"
+echo "Kirjauduttu sisään";
 return $encoded;
-
 
 ?>
