69e5f8cf00c18b176243cbca6538a67283ff65a6
Joonas Taivalmaa <Joonas.Taivalmaa@metropolia.fi>
2019-05-09 10:51:34 +0300

    Lisätty AJAX-periaatteella toimiva kirjaudu2.html

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,50 +1,37 @@
 //This script creates user object using form data.
+'use strict'
 
-function login() {
-    'use strict';
+function ajaxLogin() {
+    console.log("Käynnistetään ajaxLogin");
     var email = document.getElementById("email").value;
     var pass = document.getElementById("pass").value;
-
-    var user = {
-        email: email,
-        pass: pass
-    };
-
-    var userstring = JSON.stringify(user);
-    console.log(userstring);
-
-
-    return false;
-
-}
-function ajaxFunction() {
+    var params = "email="+email+"&pass="+pass;
     var ajaxRequest;
-    try{
-        ajaxRequest = new XMLHttpRequest();
-    } catch (e){
-        try {ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch(e){
-            try {
-                ajaxRequest = new ActiveXobject("Microsoft.XMLHTTP");
-            } catch (e) {
-                alert("Update Your Browser!");
-            }
-        }
-    }
+    ajaxRequest = new XMLHttpRequest();
 
     ajaxRequest.onreadystatechange = function() {
         if(ajaxRequest.readyState == 4) {
-            if (ajaxRequest.responseText == "yes") {
-                return true;
+            var response = ajaxRequest.responseText;
+            if (response == "true") {
+                alert("logged in");
+                localStorage.setItem("user", email);
+            }
+            else {
+                alert("ei onnistunut");
+                localStorage.setItem("user","");
             }
 
         }
     }
+    console.log("sending request with values: "+ params);
+    ajaxRequest.open("POST", "../php/login.php", true);
+    ajaxRequest.send(params);
+
 }
 
 function init() {
-    'use strict';
-    var form = document.getElementById('loginForm');
-    form.onsubmit = login;
+    var logbutton = document.getElementById('logsub');
+    var statusdiv = document.getElementById("status");
+    logbutton.onclick = ajaxLogin;
 }
 window.onload = init;
